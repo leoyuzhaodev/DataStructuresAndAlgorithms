040af84fa0208b186f6260c88c355eef2fe5c7e5
leo <leo_yuzhao_dev@aliyun.com>
2020-11-13 14:00:16 +0800

    二叉树应用：堆排序

---
+++ src/com/yzf/ch11/HeapSort.java
@@ -0,0 +1,62 @@
+package com.yzf.ch11;
+
+import java.util.Arrays;
+
+/**
+ * @description:堆排序
+ * @author:leo_yuzhao
+ * @date:2020/11/12
+ */
+public class HeapSort {
+    public static void main(String[] args) {
+        test1();
+    }
+
+    public static void test1() {
+        int array[] = new int[]{4, 6, 8, 5, 9, 3, 7};
+        heapSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void heapSort(int array[]) {
+        // 1，将数组初始化为大顶堆
+        for (int i = array.length / 2 - 1; i >= 0; i--) {
+            adjustHeap(array, i, array.length);
+        }
+        int temp = 0;
+        // 2，将进行排序：当堆顶放在数组后方，然后重新将数组调整为大顶堆。
+        for (int i = array.length - 1; i >= 1; i--) {
+            // 交换栈顶元素和最后一个元素
+            temp = array[i];
+            array[i] = array[0];
+            array[0] = temp;
+            // 调整数组为大顶堆
+            adjustHeap(array, 0, i);
+        }
+    }
+
+    /**
+     * @param arr
+     * @param i
+     * @param length
+     */
+    public static void adjustHeap(int arr[], int i, int length) {
+        int temp = arr[i];
+        for (int k = 2 * i + 1; k < length; k = 2 * k + 1) {
+            // 找出子节点中较大的一个
+            if (k + 1 < length && arr[k] < arr[k + 1]) {
+                k++;
+            }
+            if (arr[k] > temp) {
+                // 将大数移到堆顶
+                arr[i] = arr[k];
+                // i 指向的是堆顶
+                i = k;
+            } else {
+                break;
+            }
+        }
+        arr[i] = temp;
+    }
+}
+
